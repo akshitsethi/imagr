8449112fd7bec0320324f82dc052259eee98f75b
Akshit Sethi <akshitsethi@gmail.com>
2024-03-04 16:24:00 +0530

    Saving progress for switching laptops

--- bin/index.js
+++ bin/index.js
@@ -1,18 +1,57 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import fs from 'fs';
+import chalk from 'chalk';
+import boxen from 'boxen';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import Jimp from 'jimp';
 
 // Define options and args
-program
-  .version("1.0.0")
-  .description("A simple CLI application built with Node.js");
+yargs(hideBin(process.argv))
+  .command('convert <file_path>', 'Convert image to a desired format', () => {}, (argv) => {
+    let message;
 
-program.parse(process.argv);
+    const boxenOptions = {
+      padding: 1,
+      title: 'Imagr',
+      titleAlignment: 'center',
+      borderStyle: 'classic'
+    };
 
-program
-  .command("greet <name>")
-  .description("Greets the user with their name")
-  .action((name) => {
-    console.log(`Hello, ${name}!`);
-  });
+    if (!fs.existsSync(argv.file_path)) {
+      message = chalk.red(`Error: File "${argv.file_path}" does not exist. Please specify the correct path and try again.`);
+      return console.log(boxen(message, boxenOptions));
+    }
 
-program.parse(process.argv);
+    // Read file for further processing
+    Jimp.read(argv.file_path)
+      .then((image) => {
+        // Check for supported MIME
+        const mime = image.getMIME();
+
+        const supportedMimes = [
+          Jimp.MIME_JPEG,
+          Jimp.MIME_PNG,
+          Jimp.MIME_BMP,
+          Jimp.MIME_TIFF,
+          Jimp.MIME_GIF,
+          Jimp.MIME_X_MS_BMP
+        ];
+
+        if (!supportedMimes.includes(mime)) {
+          message = chalk.red(`Error: MIME for file "${argv.file_path}" is not supported. Please use a different image and try again.`);
+          return console.log(boxen(message, boxenOptions));
+        }
+
+        
+      })
+      .catch((err) => {
+        const errorMessage = err.message || 'An error occurred processing this file. Please select a different one and try again!';
+        message = chalk.red(`Error: ${errorMessage}`);
+        return console.log(boxen(message, boxenOptions));
+      });
+  })
+  .option('f', { alias: 'format', describe: 'Format of the output image', type: 'string', demandOption: true })
+  .option('s', { alias: 'size', describe: 'Desired size of the output image', type: 'string' })
+  .demandCommand(1)
+  .parse();
